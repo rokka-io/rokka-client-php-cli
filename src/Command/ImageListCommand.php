5df8d37292c1b884bf3f8edc157f08386c82540c
Emanuele Panzeri <emanuele.panzeri@liip.ch>
2017-03-02 14:20:28 +0100

    Added support for new Search options in source image listing

--- src/Command/ImageListCommand.php
+++ src/Command/ImageListCommand.php
@@ -17,6 +17,9 @@ class ImageListCommand extends BaseRokkaCliCommand
             ->setDescription('List all images stored in an organization')
             ->addOption('organization', null, InputOption::VALUE_REQUIRED, 'The organization to list images from')
             ->addOption('limit', null, InputOption::VALUE_REQUIRED, 'Limit the number of images', 20)
+            ->addOption('offset', null, InputOption::VALUE_REQUIRED, 'Offset of images to display', null)
+            ->addOption('search', null, InputOption::VALUE_IS_ARRAY | InputOption::VALUE_REQUIRED, 'Search option', [])
+            ->addOption('sort', null, InputOption::VALUE_IS_ARRAY | InputOption::VALUE_REQUIRED, 'Sorting option', [])
         ;
     }
 
@@ -29,7 +32,11 @@ class ImageListCommand extends BaseRokkaCliCommand
 
         $client = $this->clientProvider->getImageClient($organization);
         $limit = $input->getOption('limit');
-        $images = $client->listSourceImages($limit);
+        $offset = $input->getOption('offset');
+        $search = $this->buildSearchParameter($input->getOption('search'));
+        $sorts = $this->buildSortParameter($input->getOption('sort'));
+
+        $images = $client->searchSourceImages($search, $sorts, $limit, $offset);
 
         $table = new Table($output);
         $table->setHeaders(['Name', 'HASH', 'Organization', 'Created', 'Size']);
@@ -48,5 +55,54 @@ class ImageListCommand extends BaseRokkaCliCommand
         $table->render();
 
         return 0;
+    }
+
+    /**
+     * Builds the "search" parameter as required bu the Rokka client.
+     *
+     * @param array $searchFilters
+     *
+     * @return array
+     */
+    protected function buildSearchParameter(array $searchFilters)
+    {
+        $search = [];
+        foreach ($searchFilters as $filter) {
+            $parts = explode(' ', trim($filter), 2);
+
+            if (empty($parts) || 2 !== count($parts)) {
+                continue;
+            }
+            $search[$parts[0]] = $parts[1];
+        }
+
+        return $search;
+    }
+
+    /**
+     * Builds the "sort" parameter as required bu the Rokka client.
+     *
+     * @param array $sorts
+     *
+     * @return array
+     */
+    protected function buildSortParameter(array $sorts)
+    {
+        $sorting = [];
+        foreach ($sorts as $sort) {
+            $parts = explode(' ', trim($sort), 2);
+
+            if (empty($parts)) {
+                continue;
+            }
+
+            if (1 === count($parts)) {
+                $sorting[$parts[0]] = true;
+            } else {
+                $sorting[$parts[0]] = $parts[1];
+            }
+        }
+
+        return $sorting;
     }
 }
